7a32c2c927d73c0fca75391bc0879087f2f36ed8
David Zuckman <37224202+dzuckman@users.noreply.github.com>
2023-09-25 22:00:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents Healthy New Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Eggs')
-streamlit.text('Bacon')
-streamlit.text('Roast Beans')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
