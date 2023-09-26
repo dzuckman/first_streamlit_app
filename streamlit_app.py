28da789459805034df850fec842dbfa9048735e0
David Zuckman <37224202+dzuckman@users.noreply.github.com>
2023-09-25 21:59:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents Healthy New Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Eggs')
+streamlit.text('Bacon')
+streamlit.text('Roast Beans')
