d9dc81a3319f4e0f4bf0df812e2bb18da9870cb9
David Zuckman <37224202+dzuckman@users.noreply.github.com>
2023-09-25 22:08:19 -0400

    Update streamlit_app.py\

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,7 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast you stinking millennial')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.dataframe(my_fruit_list)
-
-# Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-
-# Display the table on the page.
+streamlit.dataframe(my_fruit_list)
